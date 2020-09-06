428d8d4cc1b3061236bcc767d85385f634a4308b
Sumito Izumita <sumito@izumita.com>
2020-09-06 10:51:36 +0900

    テストの追加

--- test/helper_test.exs
+++ test/helper_test.exs
@@ -5,4 +5,12 @@ defmodule HelperTest do
     assert "りんごはりんご、みかんはみかん。" |> Helper.duplication_words == ["リンゴ", "ミカン"]
   end
 
+  test "get duplication words from `私はリンゴです。`" do
+    assert "私はリンゴです。" |> Helper.duplication_words == []
+  end
+
+  test "get duplication words from `りんごはリンゴだけど、すももは桃じゃない。`" do
+    assert "りんごはリンゴだけど、すももは桃じゃない。" |> Helper.duplication_words == ["リンゴ"]
+  end
+
 end
