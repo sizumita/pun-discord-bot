f5cd74e49193babd5eb3b50b13600e8e67f7a9c4
Sumito Izumita <sumito@izumita.com>
2020-09-07 15:13:02 +0900

    inviteコマンドの追加

--- lib/pun_discord_bot/command.ex
+++ lib/pun_discord_bot/command.ex
@@ -1,13 +1,8 @@
 defmodule PunDiscordBot.Command do
   alias PunDiscordBot.Command
   @prefix "djr!"
-  @bot_id 752040386104655922
-
-  def handle(%{author: %{id: @bot_id}}), do: :noop
-
-  def handle(msg = %{content: @prefix <> "parse " <> content}) do
-    create_message msg.channel_id, "```\n#{Parser.get_yomi(content)}\n```"
-  end
+  @mention "<@752040386104655922> "
+  @mention_nick "<@!#{Application.get_env(:nostrum, :bot_id)}> "
 
   def handle(msg = %{content: @prefix <> content}) do
     content
@@ -15,9 +10,21 @@ defmodule PunDiscordBot.Command do
     |> execute(msg)
   end
 
+  def handle(msg = %{content: @mention <> content}) do
+    content
+    |> String.trim()
+    |> execute(msg)
+  end
+
+  def handle(msg = %{content: @mention_nick <> content}) do
+    content
+    |> String.trim()
+    |> execute(msg)
+  end
+
   def handle(msg) do
     pun = Pun.search msg.content
-    if pun.surface != "" do
+    if msg.author.bot && pun.surface != "" do
       create_message msg.channel_id, "だじゃれを検出しました！\n> **#{pun.surface}**"
     end
   end
@@ -26,8 +33,11 @@ defmodule PunDiscordBot.Command do
     create_message msg.channel_id, "pong"
   end
 
+  defp execute("invite", msg) do
+    create_message msg.channel_id, "https://discord.com/api/oauth2/authorize?client_id=752040386104655922&permissions=0&scope=bot"
+  end
+
   defp execute(_, msg) do
-    create_message msg.channel_id, "This command doesnt exist, sorry"
   end
 
   defp create_message(channel_id, message) do
